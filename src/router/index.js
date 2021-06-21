9c89bdfa5a8f49d19184324c9e1a3ea4a841f5fa
Boluwatife2904 <sanvicola2000@gmail.com>
2021-06-21 07:32:10 +0100

    Removed default components and styles that wasn't needed, added few stylings of mine and general setup overall

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Welcome from '../views/Welcome.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    name: 'Welcome',
+    component: Welcome
   }
 ]
 
---
+++ src/views/Welcome.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="welcome container">
+    <p>Welcome</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Welcome",
+}
+</script>
+
+<style lang="scss" scoped>
+.welcome {
+  text-align: center;
+  padding: 20px 0;
+}
+</style>
