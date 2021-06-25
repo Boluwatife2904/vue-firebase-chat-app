7b2e699e9864614bfe5fbfffe4209049cc1337e3
Boluwatife2904 <sanvicola2000@gmail.com>
2021-06-25 09:28:07 +0100

    Added dynamic document titles, theme-color, favicon and description

--- src/router/index.js
+++ src/router/index.js
@@ -16,20 +16,31 @@ const routes = [
   {
     path: '/',
     name: 'Welcome',
-    component: Welcome
+    component: Welcome,
+    meta: {
+      title: "Welcome || RoomsHouse"
+    }
   }, 
   {
     path: "/chatroom/:room",
     name: "Chatroom",
     component: Chatroom,
     props: true,
-    beforeEnter: requiresAuthentication
+    beforeEnter: requiresAuthentication,
+    meta: {
+      title: "Chatroom || RoomsHouse"
+    }
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach((to, _, next) => {
+  document.title = to.meta.title;
+  next();
 })
 
 export default router
